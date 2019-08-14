ca1b8f2ef69110d37e7042e7cd23c896b5100681
Mohini Swapnil Wagh <swapwagh@gmail.com>
2019-08-13 21:54:43 -0700

    Implemented a restful endpoint GET /filteredimage?image_url={{URL}}

---
+++ course-02/project/image-filter-starter-code/src/filteredimage/filteredimage.router.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.get( "/filteredimage/", ( req: Request, res: Response ) => {
+    let { name } = req.query;
+
+    if ( !name ) {
+      return res.status(400)
+                .send(`name is required`);
+    }
+
+    return res.status(200)
+              .send(`Welcome to the Cloud, ${name}!`);
+  } );
--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,9 @@
-import express from 'express';
+import express, { response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { TimeoutError } from 'bluebird';
+const isImageUrl = require('is-image-url');
+const axios = require('axios');
 
 (async () => {
 
@@ -35,7 +38,48 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
+  });
+  
+  app.get( "/filteredimage", async ( req, res ) => {
+    let {image_url}= req.query;
+      
+      //console.log(image_url);
+      axios({
+        method: 'get',
+        url: image_url,
+        timeout: 1000 * 5, // Wait for 5 seconds
+      })
+        .then(function (response: any) {
+          const headers = response.headers;
+          if (headers && headers['content-type']) {
+            const isImage =  headers['content-type'].search(/^image\//) != -1; //    1. validate the image_url query
+          if (isImage) {
+            filterImageFromURL(image_url).then( response => { //    2. call filterImageFromURL(image_url) to filter the image
+              //console.log(response);
+             var path: any[] | string[]=[];
+             path[path.length]=response;
+            //res.sendFile(response); //    3. send the resulting file in the response
+            
+            res.sendFile(response, function (error) {
+              if (error) {
+                res.send("There was an error saving the image to /tmp")
+              } else {
+                deleteLocalFiles(path); //    4. deletes any files on the server on finish of the response
+                console.log('Sent:', response)
+              }
+            })
+
+                }).catch(error => {
+                  res.send("There was an error saving the image to /tmp")
+                })
+          } else {
+            res.send("The url is not an image")
+          }
+          }
+          }).catch((error: any) => {
+          res.send("There was an error accessing the image")
+        });
+    } );
   
 
   // Start the Server
